eafce6e74668acf3e25d21d4143fae21b5717608
jordan4ibanez <jordan4ibanez@users.noreply.github.com>
2024-09-18 11:43:52 -0400

    move it

--- src/uuid_now.f90
+++ src/uuid_now.f90
@@ -9,24 +9,7 @@ module uuid_now
   integer(c_int64_t), parameter :: MIN_64 = -9223372036854775807_8 - 1
 
 
-  integer(c_int64_t) :: current = MIN_64
 
-  interface
-
-
-    ! smol debug
-    subroutine print_bits(int) bind(c, name = "print_bits")
-      use, intrinsic :: iso_c_binding
-      !! Wrap the function internal to make raw binding.
-      implicit none
-
-      ! Value because it is const char *
-
-      integer(c_int64_t), intent(in), value :: int
-    end subroutine print_bits
-
-
-  end interface
 
 
 contains
